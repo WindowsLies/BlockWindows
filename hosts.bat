44eab34092bdc472e7ccb2ae8633e282a875bb1b
WindowsLies <windowslies@Safe-mail.net>
2015-08-26 02:49:15 +0000

    Blocks MS Telemetry IP's. Appends to current hosts file. Run as Admin

--- hosts.bat
+++ hosts.bat
@@ -1,4 +1,4 @@
-REM This blocks MS telemetry IP's from hosts file. Appends to current hosts file, only run once 
+REM This blocks MS telemetry IP's from hosts file. Appends to current hosts file, only run once
 REM Windows 10 bypasses this, if running WIN10 add the entries in the file 'hostslist' to your router or firewall blocklist
 REM Put all files in your Downloads directory. Right click and run this file as administrator
 type %homepath%\Downloads\hosts >> %windir%\system32\Drivers\etc\hosts
