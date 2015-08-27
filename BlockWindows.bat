8806a014ec628e4c2cc7d0f3707bf57c916ba034
Victor Benincasa <vbenincasa@gmail.com>
2015-08-27 05:56:35 -0300

    Update BlockWindows.bat
    Small fixes and improvements.

--- BlockWindows.bat
+++ BlockWindows.bat
@@ -7,14 +7,17 @@ echo uninstalling updates ...
 echo Delete KB971033 (license validation)
 start "title" /b /wait wusa.exe /kb:971033 /uninstall /quiet /norestart
 echo  - next
-echo Delete KB2902907 (description not available)
+echo Delete KB2902907 (Microsoft Security Essentials)
 start "title" /b /wait wusa.exe /kb:2902907 /uninstall /quiet /norestart
 echo  - next
 echo Delete KB2952664 (Get Windows 10 assistant)
 start "title" /b /wait wusa.exe /kb:2952664 /uninstall /quiet /norestart
 echo  - next
-echo Delete KB2976987 (description not available)
-start "title" /b /wait wusa.exe /kb:2976987 /uninstall /quiet /norestart
+echo Delete KB2976978 (Compatibility update for Windows 8.1 and Windows 8)
+start "title" /b /wait wusa.exe /kb:2976978 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB2977759 (Compatibility update for Windows 7 RTM)
+start "title" /b /wait wusa.exe /kb:2977759 /uninstall /quiet /norestart
 echo  - next
 echo Delete KB2990214 (Get Windows 10 for Win7)
 start "title" /b /wait wusa.exe /kb:2990214 /uninstall /quiet /norestart
@@ -34,20 +37,11 @@ echo  - next
 echo Delete KB3044374 (Get Windows 10 for Win8.1)
 start "title" /b /wait wusa.exe /kb:3044374 /uninstall /quiet /norestart
 echo  - next
-echo Delete KB3050265 (update for Windows Update on Win7)
-start "title" /b /wait wusa.exe /kb:3050265 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3065987 (update for Windows Update on Win7/Server 2008R2)
-start "title" /b /wait wusa.exe /kb:3065987 /uninstall /quiet /norestart
-echo  - next
 echo Delete KB3068708 (telemetry)
 start "title" /b /wait wusa.exe /kb:3068708 /uninstall /quiet /norestart
 echo  - next
 echo Delete KB3075249 (telemetry for Win7/8.1)
 start "title" /b /wait wusa.exe /kb:3075249 /uninstall /quiet /norestart
-echo  - next
-echo Delete KB3075853 (update for Windows Update on Win8.1/Server 2012R2)
-start "title" /b /wait wusa.exe /kb:3075853 /uninstall /quiet /norestart
 echo  - next
 echo Delete KB3080149 (Telemetry)
 start "title" /b /wait wusa.exe /kb:3080149 /uninstall /quiet /norestart
@@ -56,9 +50,10 @@ timeout 5
 
 REM --- Hide updates
 echo Hiding updates ...
-start "title" /b /wait cscript.exe "%~dp0HideWindowsUpdates.vbs" 971033 2902907 2952664 2976987 2990214 3012973 3021917 3022345 3035583 3044374 3050265 3065987 3068708 3075249 3075853 3080149 
+start "title" /b /wait cscript.exe "%~dp0HideWindowsUpdates.vbs" 971033 2902907 2952664 2976978 2977759 2990214 3012973 3021917 3022345 3035583 3044374 3068708 3075249 3080149 
 echo  - done.
 
+REM --- Block Routes
 echo Blocking Routes…
 route -p add 23.218.212.69 MASK 255.255.255.255 0.0.0.0
 route -p add 65.55.108.23 MASK 255.255.255.255 0.0.0.0
@@ -67,38 +62,40 @@ route -p add 134.170.30.202 MASK 255.255.255.255 0.0.0.0
 route -p add 137.116.81.24 MASK 255.255.255.255 0.0.0.0
 route -p add 204.79.197.200 MASK 255.255.255.255 0.0.0.0
 route -p add 23.218.212.69 MASK 255.255.255.255 0.0.0.0
-echo - done
+
+REM --- Disable tasks
 echo Disabling tasks…
-schtasks /Change /TN “\Microsoft\Windows\Application Experience\AitAgent” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Application Experience\ProgramDataUpdater” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Autochk\Proxy” /DISABLE
-schtasks /Change /TN “Microsoft\Windows\Customer Experience Improvement Program\Consolidator” /DISABLE
-schtasks /Change /TN “Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask” /DISABLE
-schtasks /Change /TN “Microsoft\Windows\Customer Experience Improvement Program\UsbCeip” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Maintenance\WinSAT” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\ActivateWindowsSearch” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\ConfigureInternetTimeService” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\DispatchRecoveryTasks” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\ehDRMInit” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\InstallPlayReady” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\mcupdate” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\MediaCenterRecoveryTask” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\ObjectStoreRecoveryTask” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\OCURActivate” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\OCURDiscovery” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\PBDADiscovery” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\PBDADiscoveryW1” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\PBDADiscoveryW2” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\PvrRecoveryTask” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\PvrScheduleTask” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\RegisterSearch” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\ReindexSearchRoot” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\SqlLiteRecoveryTask” /DISABLE
-schtasks /Change /TN “\Microsoft\Windows\Media Center\UpdateRecordPath” /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\AitAgent" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\ProgramDataUpdater" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Autochk\Proxy" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Maintenance\WinSAT" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\ActivateWindowsSearch" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\ConfigureInternetTimeService" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\DispatchRecoveryTasks" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\ehDRMInit" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\InstallPlayReady" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\mcupdate" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\MediaCenterRecoveryTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\ObjectStoreRecoveryTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\OCURActivate" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\OCURDiscovery" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\PBDADiscovery" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\PBDADiscoveryW1" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\PBDADiscoveryW2" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\PvrRecoveryTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\PvrScheduleTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\RegisterSearch" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\ReindexSearchRoot" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\SqlLiteRecoveryTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\UpdateRecordPath" /DISABLE
 echo - done
 
+REM --- Kill services
 echo Killing Diagtrack-service (if it still exists)…
 sc stop Diagtrack
 sc delete Diagtrack
