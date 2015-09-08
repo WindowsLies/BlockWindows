5994c1ef5ed5ff36ba6054ced4556c63301415a9
WindowsLies <windowslies@Safe-mail.net>
2015-09-08 00:10:02 +0000

    Unblocks hosts2.bat blocking

---
+++ unblock.bat
@@ -0,0 +1,526 @@
+REM --- unblock Hosts
+echo unblocking Hosts(Including OUTLOOK,HOTMAIL,LIVE.COM,BING)
+timeout 4
+
+echo unblock a-0001.a-msedge.net
+route -p delete 204.79.197.200/32 127.0.0.1
+
+echo unblock a23-218-212-69.deploy.static.akamaitechnologies.com
+route -p delete 23.218.212.69/32 127.0.0.1
+
+echo unblock a.ads1.msn.com
+route -p delete 204.160.124.125/32 127.0.0.1
+route -p delete 8.253.14.126/32 127.0.0.1
+route -p delete 8.254.25.126/32 127.0.0.1
+
+echo unblock a.ads2.msads.net
+route -p delete 93.184.215.200/32 127.0.0.1
+
+echo unblock a.ads2.msn.com
+route -p delete 198.78.194.252/32 127.0.0.1
+route -p delete 198.78.209.253/32 127.0.0.1
+route -p delete 8.254.23.254/32 127.0.0.1
+
+echo unblock ac3.msn.com
+route -p delete 131.253.14.76/32 127.0.0.1
+
+echo unblock activation.playready.microsoft.com
+route -p delete 134.170.119.205/32 127.0.0.1
+
+echo unblock ads1.msads.net
+route -p delete 23.201.58.73/32 127.0.0.1
+
+echo unblock ads1.msn.com
+route -p delete 204.160.124.125/32 127.0.0.1
+route -p delete 8.253.14.126/32 127.0.0.1
+route -p delete 8.254.25.126/32 127.0.0.1
+
+echo unblock adsmockarc.azurewebsites.net
+route -p delete 191.236.16.12/32 127.0.0.1
+
+echo unblock ads.msn.com
+route -p delete 157.56.91.82/32 127.0.0.1
+
+echo unblock adsyndication.msn.com
+route -p delete 207.46.120.187/32 127.0.0.1
+
+echo unblock ajax.aspnetcdn.com
+route -p delete 93.184.215.200/32 127.0.0.1
+
+echo unblock api.bing.com
+route -p delete 191.234.5.80/32 127.0.0.1
+
+echo unblock appexmapsappupdate.blob.core.windows.net
+route -p delete 23.98.49.14/32 127.0.0.1
+
+echo unblock apps.skype.com
+route -p delete 104.79.147.136/32 127.0.0.1
+
+echo unblock a.rad.msn.com
+route -p delete 65.55.2.6/32 127.0.0.1
+
+echo unblock au.au-msedge.net
+route -p delete 191.234.4.50/32 127.0.0.1
+
+echo unblock auth.gfx.ms
+route -p delete 23.61.72.70/32 127.0.0.1
+
+echo unblock b.ads1.msn.com
+route -p delete 204.160.124.125/32 127.0.0.1
+route -p delete 8.253.14.126/32 127.0.0.1
+route -p delete 8.254.25.126/32 127.0.0.1
+
+echo unblock b.ads2.msads.net
+route -p delete 93.184.215.200/32 127.0.0.1
+
+echo unblock bing.com
+route -p delete 204.79.197.200/32 127.0.0.1
+
+echo unblock bingads.microsoft.com
+route -p delete 66.119.152.205/32 127.0.0.1
+
+echo unblock b.rad.msn.com
+route -p delete 65.55.2.6/32 127.0.0.1
+
+echo unblock bs.serving-sys.com
+route -p delete 63.241.108.124/32 127.0.0.1
+
+echo unblock c1.microsoft.com
+route -p delete 131.253.40.50/32 127.0.0.1
+
+echo unblock cdn.content.prod.cms.msn.com
+route -p delete 165.254.155.67/32 127.0.0.1
+route -p delete 165.254.155.75/32 127.0.0.1
+
+echo unblock cdp1.public-trust.com
+route -p delete 64.18.20.10/32 127.0.0.1
+route -p delete 65.207.25.151/32 127.0.0.1
+
+echo unblock choice.microsoft.com
+route -p delete 157.56.91.77/32 127.0.0.1
+
+echo unblock choice.microsoft.com.nsatc.net
+route -p delete 157.56.91.77/32 127.0.0.1
+
+echo unblock c.microsoft.com
+route -p delete 134.170.185.126/32 127.0.0.1
+
+echo unblock c.msn.com
+route -p delete 131.253.40.50/32 127.0.0.1
+
+echo unblock cmsresources.windowsphone.com
+route -p delete 23.201.59.128/32 127.0.0.1
+
+echo unblock content.windows.microsoft.com
+route -p delete 165.254.155.49/32 127.0.0.1
+route -p delete 165.254.155.96/32 127.0.0.1
+
+echo unblock corpext.msitadfs.glbdns2.microsoft.com
+route -p delete 65.55.29.238/32 127.0.0.1
+
+echo unblock corp.sts.microsoft.com
+route -p delete 65.55.29.238/32 127.0.0.1
+
+echo unblock c.s-microsoft.com
+route -p delete 23.201.36.177/32 127.0.0.1
+
+echo unblock dc.services.visualstudio.com
+route -p delete 23.102.169.243/32 127.0.0.1
+
+echo unblock dev.virtualearth.net
+route -p delete 131.253.40.84/32 127.0.0.1
+
+echo unblock df.telemetry.microsoft.com
+route -p delete 65.52.100.7/32 127.0.0.1
+
+echo unblock diagnostics.support.microsoft.com
+route -p delete 157.56.121.89/32 127.0.0.1
+
+echo unblock displaycatalog.md.mp.microsoft.com
+route -p delete 64.4.54.50/32 127.0.0.1
+
+echo unblock dl.delivery.mp.microsoft.com
+route -p delete 165.254.206.121/32 127.0.0.1
+route -p delete 165.254.206.122/32 127.0.0.1
+
+echo unblock dmd.metaservices.microsoft.com
+route -p delete 134.170.30.221/32 127.0.0.1
+
+echo unblock download.microsoft.com
+route -p delete 23.74.8.194/32 127.0.0.1
+route -p delete 23.74.8.209/32 127.0.0.1
+
+echo unblock download-ssl.msgamestudios.com
+route -p delete 104.79.151.241/32 127.0.0.1
+
+echo unblock ecn.dev.virtualearth.net
+route -p delete 23.201.59.4/32 127.0.0.1
+
+echo unblock en-us.appex-rf.msn.com
+route -p delete 165.254.155.81/32 127.0.0.1
+route -p delete 165.254.155.88/32 127.0.0.1
+
+echo unblock fe3.delivery.mp.microsoft.com
+route -p delete 65.52.108.90/32 127.0.0.1
+
+echo unblock feedback.microsoft-hohm.com
+route -p delete 64.4.6.100/32 127.0.0.1
+route -p delete 65.55.39.10/32 127.0.0.1
+
+echo unblock feedback.search.microsoft.com
+route -p delete 157.55.129.21/32 127.0.0.1
+
+echo unblock feedback.windows.com
+route -p delete 134.170.188.248/32 127.0.0.1
+
+echo unblock ff4a487e56259f4bd5831e9e30470e83.azr.msnetworkanalytics.testanalytics.net
+route -p delete 65.52.8.54/32 127.0.0.1
+
+echo unblock flex.msn.com
+route -p delete 131.253.40.47/32 127.0.0.1
+
+echo unblock geo-prod.do.dsp.mp.microsoft.com
+route -p delete 64.4.54.98/32 127.0.0.1
+
+echo unblock g.live.com
+route -p delete 131.253.40.59/32 127.0.0.1
+
+echo unblock g.msn.com
+route -p delete 131.253.40.59/32 127.0.0.1
+
+echo unblock go.microsoft.com
+route -p delete 104.79.133.115/32 127.0.0.1
+
+echo unblock go.skype.com
+route -p delete 191.234.42.225/32 127.0.0.1
+
+echo unblock h1.msn.com
+route -p delete 23.201.54.238/32 127.0.0.1
+
+echo unblock h2.msn.com
+route -p delete 23.201.54.238/32 127.0.0.1
+
+echo unblock help.bingads.microsoft.com
+route -p delete 207.46.202.114/32 127.0.0.1
+
+echo unblock hotmail.com
+route -p delete 157.55.152.112/32 127.0.0.1
+route -p delete 157.56.172.28/32 127.0.0.1
+route -p delete 65.55.77.28/32 127.0.0.1
+route -p delete 65.55.85.12/32 127.0.0.1
+
+echo unblock i1.services.social.microsoft.com
+route -p delete 23.201.43.90/32 127.0.0.1
+
+echo unblock i1.services.social.microsoft.com.nsatc.net
+
+echo unblock iecvlist.microsoft.com
+route -p delete 93.184.215.200/32 127.0.0.1
+
+echo unblock ieonline.microsoft.com
+route -p delete 204.79.197.200/32 127.0.0.1
+
+echo unblock img-s-msn-com.akamaized.net
+route -p delete 165.254.155.48/32 127.0.0.1
+route -p delete 165.254.155.56/32 127.0.0.1
+
+echo unblock inference.location.live.net
+route -p delete 191.238.27.70/32 127.0.0.1
+
+echo unblock js.microsoft.com
+route -p delete 23.201.36.177/32 127.0.0.1
+
+echo unblock licensing.md.mp.microsoft.com
+route -p delete 64.4.54.36/32 127.0.0.1
+
+echo unblock live.rads.msn.com
+route -p delete 137.117.100.176/32 127.0.0.1
+
+echo unblock livetileedge.dsx.mp.microsoft.com
+route -p delete 104.79.150.111/32 127.0.0.1
+
+echo unblock logging.windows.microsoft.com
+route -p delete 134.170.119.140/32 127.0.0.1
+
+echo unblock login.live.com
+route -p delete 131.253.61.66/32 127.0.0.1
+route -p delete 131.253.61.68/32 127.0.0.1
+route -p delete 131.253.61.80/32 127.0.0.1
+route -p delete 131.253.61.98/32 127.0.0.1
+
+echo unblock login.live.com.nsatc.net
+route -p delete 131.253.61.66/32 127.0.0.1
+route -p delete 131.253.61.68/32 127.0.0.1
+route -p delete 131.253.61.80/32 127.0.0.1
+route -p delete 131.253.61.98/32 127.0.0.1
+
+echo unblock mediadiscovery.microsoft.com
+route -p delete 104.79.153.14/32 127.0.0.1
+
+echo unblock m.hotmail.com
+route -p delete 207.46.101.8/32 127.0.0.1
+
+echo unblock microsoft.com
+route -p delete 134.170.185.46/32 127.0.0.1
+route -p delete 134.170.188.221/32 127.0.0.1
+
+echo unblock msftncsi.com
+
+echo unblock msnbc.com
+route -p delete 23.78.45.86/32 127.0.0.1
+
+echo unblock msn.com
+route -p delete 23.101.196.141/32 127.0.0.1
+
+echo unblock msnportal.112.2o7.net
+route -p delete 192.243.250.72/32 127.0.0.1
+route -p delete 192.243.250.88/32 127.0.0.1
+route -p delete 66.235.138.193/32 127.0.0.1
+route -p delete 66.235.138.194/32 127.0.0.1
+route -p delete 66.235.138.195/32 127.0.0.1
+route -p delete 66.235.139.17/32 127.0.0.1
+route -p delete 66.235.139.18/32 127.0.0.1
+route -p delete 66.235.139.19/32 127.0.0.1
+route -p delete 66.235.139.205/32 127.0.0.1
+route -p delete 66.235.139.206/32 127.0.0.1
+route -p delete 66.235.139.207/32 127.0.0.1
+
+echo unblock msntest.serving-sys.com
+route -p delete 128.177.96.56/32 127.0.0.1
+route -p delete 128.177.96.59/32 127.0.0.1
+
+echo unblock nexus.officeapps.live.com
+route -p delete 104.46.50.125/32 127.0.0.1
+
+echo unblock oca.telemetry.microsoft.com
+route -p delete 65.55.252.63/32 127.0.0.1
+
+echo unblock oca.telemetry.microsoft.com.nsatc.net
+route -p delete 65.55.252.63/32 127.0.0.1
+
+echo unblock ocsp.msocsp.com
+route -p delete 108.162.232.196/32 127.0.0.1
+route -p delete 108.162.232.197/32 127.0.0.1
+route -p delete 108.162.232.198/32 127.0.0.1
+route -p delete 108.162.232.199/32 127.0.0.1
+route -p delete 108.162.232.200/32 127.0.0.1
+route -p delete 108.162.232.201/32 127.0.0.1
+route -p delete 108.162.232.202/32 127.0.0.1
+route -p delete 108.162.232.203/32 127.0.0.1
+route -p delete 108.162.232.204/32 127.0.0.1
+route -p delete 108.162.232.205/32 127.0.0.1
+route -p delete 108.162.232.207/32 127.0.0.1
+
+echo unblock ocsp.omniroot.com
+route -p delete 72.21.91.8/32 127.0.0.1
+
+echo unblock ocsp.startssl.com
+route -p delete 165.254.114.112/32 127.0.0.1
+route -p delete 165.254.114.120/32 127.0.0.1
+
+echo unblock officeclient.microsoft.com
+route -p delete 137.116.64.35/32 127.0.0.1
+
+echo unblock outlook.com
+route -p delete 132.245.113.194/32 127.0.0.1
+route -p delete 132.245.13.210/32 127.0.0.1
+route -p delete 132.245.17.34/32 127.0.0.1
+route -p delete 132.245.23.242/32 127.0.0.1
+route -p delete 132.245.81.130/32 127.0.0.1
+route -p delete 132.245.92.194/32 127.0.0.1
+route -p delete 157.56.237.242/32 127.0.0.1
+
+echo unblock pipe.skype.com
+route -p delete 104.43.161.140/32 127.0.0.1
+
+echo unblock platform.bing.com
+route -p delete 204.79.197.200/32 127.0.0.1
+
+echo unblock pre.footprintpredict.com
+route -p delete 204.79.197.200/32 127.0.0.1
+
+echo unblock preview.msn.com
+route -p delete 137.117.100.176/32 127.0.0.1
+
+echo unblock pricelist.skype.com
+
+echo unblock r20swj13mr.microsoft.com
+route -p delete 93.184.215.200/32 127.0.0.1
+
+echo unblock rad.live.com
+route -p delete 65.55.2.6/32 127.0.0.1
+
+echo unblock rad.msn.com
+route -p delete 65.55.2.6/32 127.0.0.1
+
+echo unblock redir.metaservices.microsoft.com
+route -p delete 165.254.155.51/32 127.0.0.1
+route -p delete 165.254.155.88/32 127.0.0.1
+
+echo unblock reports.wes.df.telemetry.microsoft.com
+route -p delete 65.52.100.91/32 127.0.0.1
+
+echo unblock sb.scorecardresearch.com
+route -p delete 104.79.156.195/32 127.0.0.1
+
+echo unblock services.wes.df.telemetry.microsoft.com
+route -p delete 65.52.100.92/32 127.0.0.1
+
+echo unblock settings.data.microsoft.com
+route -p delete 65.55.44.108/32 127.0.0.1
+
+echo unblock settings-sandbox.data.microsoft.com
+route -p delete 111.221.29.177/32 127.0.0.1
+
+echo unblock settings-ssl.xboxlive.com
+route -p delete 104.79.156.227/32 127.0.0.1
+
+echo unblock settings-win.data.microsoft.com
+route -p delete 65.55.44.108/32 127.0.0.1
+
+echo unblock s.gateway.messenger.live.com
+route -p delete 157.56.106.210/32 127.0.0.1
+
+echo unblock sgmetrics.cloudapp.net
+route -p delete 168.62.11.145/32 127.0.0.1
+
+echo unblock shell.windows.com
+route -p delete 137.116.81.94/32 127.0.0.1
+
+echo unblock sls.update.microsoft.com
+route -p delete 157.55.133.204/32 127.0.0.1
+
+echo unblock sls.update.microsoft.com.akadns.net
+route -p delete 157.55.240.220/32 127.0.0.1
+
+echo unblock sls.update.microsoft.com.nsatc.net
+route -p delete 157.55.133.204/32 127.0.0.1
+
+echo unblock social.answers.microsoft.com
+route -p delete 134.170.52.190/32 127.0.0.1
+
+echo unblock spynet2.microsoft.com
+route -p delete 23.96.212.225/32 127.0.0.1
+
+echo unblock spynetalt.microsoft.com
+route -p delete 23.96.212.225/32 127.0.0.1
+
+echo unblock sqm.df.telemetry.microsoft.com
+route -p delete 65.52.100.94/32 127.0.0.1
+
+echo unblock sqm.microsoft.com
+route -p delete 65.55.44.82/32 127.0.0.1
+
+echo unblock sqm.telemetry.microsoft.com
+route -p delete 65.55.252.93/32 127.0.0.1
+
+echo unblock sqm.telemetry.microsoft.com.nsatc.net
+route -p delete 65.55.252.93/32 127.0.0.1
+
+echo unblock ssw.live.com
+route -p delete 207.46.101.29/32 127.0.0.1
+
+echo unblock statsfe1.ws.microsoft.com
+route -p delete 134.170.115.60/32 127.0.0.1
+route -p delete 207.46.114.61/32 127.0.0.1
+
+echo unblock statsfe2.update.microsoft.com.akadns.net
+route -p delete 65.52.108.153/32 127.0.0.1
+
+echo unblock statsfe2.ws.microsoft.com
+route -p delete 64.4.54.22/32 127.0.0.1
+
+echo unblock storeedgefd.dsx.mp.microsoft.com
+route -p delete 104.79.153.53/32 127.0.0.1
+
+echo unblock store-images.microsoft.com
+route -p delete 104.79.129.31/32 127.0.0.1
+
+echo unblock survey.watson.microsoft.com
+route -p delete 207.68.166.254/32 127.0.0.1
+
+echo unblock t0.ssl.ak.dynamic.tiles.virtualearth.net
+route -p delete 104.79.157.54/32 127.0.0.1
+
+echo unblock t0.ssl.ak.tiles.virtualearth.net
+route -p delete 23.201.29.83/32 127.0.0.1
+
+echo unblock telecommand.telemetry.microsoft.com
+route -p delete 65.55.252.92/32 127.0.0.1
+
+echo unblock telecommand.telemetry.microsoft.com.nsatc.net
+route -p delete 65.55.252.92/32 127.0.0.1
+
+echo unblock telemetry.appex.bing.net
+route -p delete 168.62.187.13/32 127.0.0.1
+
+echo unblock telemetry.microsoft.com
+route -p delete 65.52.100.9/32 127.0.0.1
+
+echo unblock telemetry.urs.microsoft.com
+route -p delete 131.253.40.37/32 127.0.0.1
+
+echo unblock tile-service.weather.microsoft.com
+route -p delete 23.201.54.135/32 127.0.0.1
+
+echo unblock tlu.dl.delivery.mp.microsoft.com
+route -p delete 165.254.206.130/32 127.0.0.1
+route -p delete 165.254.206.137/32 127.0.0.1
+
+echo unblock ui.skype.com
+route -p delete 157.56.109.8/32 127.0.0.1
+
+echo unblock urs.microsoft.com
+route -p delete 134.170.21.248/32 127.0.0.1
+
+echo unblock vassg141.ocsp.omniroot.com
+route -p delete 209.133.57.74/32 127.0.0.1
+route -p delete 209.133.57.83/32 127.0.0.1
+
+echo unblock version.hybrid.api.here.com
+route -p delete 107.20.234.199/32 127.0.0.1
+route -p delete 174.129.23.235/32 127.0.0.1
+route -p delete 174.129.30.42/32 127.0.0.1
+route -p delete 54.243.53.207/32 127.0.0.1
+
+echo unblock vortex.data.microsoft.com
+route -p delete 64.4.54.254/32 127.0.0.1
+
+echo unblock vortex-sandbox.data.microsoft.com
+route -p delete 64.4.54.32/32 127.0.0.1
+
+echo unblock vortex-win.data.microsoft.com
+route -p delete 64.4.54.254/32 127.0.0.1
+
+echo unblock watson.live.com
+route -p delete 207.46.223.94/32 127.0.0.1
+
+echo unblock watson.microsoft.com
+route -p delete 65.55.252.71/32 127.0.0.1
+
+echo unblock watson.ppe.telemetry.microsoft.com
+route -p delete 65.52.100.11/32 127.0.0.1
+
+echo unblock watson.telemetry.microsoft.com
+route -p delete 65.52.108.29/32 127.0.0.1
+
+echo unblock watson.telemetry.microsoft.com.nsatc.net
+route -p delete 65.52.108.29/32 127.0.0.1
+
+echo unblock wes.df.telemetry.microsoft.com
+route -p delete 65.52.100.93/32 127.0.0.1
+
+echo unblock www.microsoft.com
+route -p delete 184.31.194.59/32 127.0.0.1
+
+echo unblock www.modern.ie
+route -p delete 168.62.49.33/32 127.0.0.1
+
+echo unblock www.msftncsi.com
+route -p delete 128.177.96.24/32 127.0.0.1
+route -p delete 128.177.96.65/32 127.0.0.1
+
+echo unblock www.msn.com
+route -p delete 204.79.197.203/32 127.0.0.1
+
+echo done
